697bc0030bb3f8337df2c8ee2b09f65c25e1d579
HoneyryderChuck <cardoso_tiago@hotmail.com>
2021-03-25 19:38:47 +0000

    forcing all libcurl clients too http/1.1 for single and persistent benchmarks

--- clients/curb.rb
+++ clients/curb.rb
@@ -10,11 +10,22 @@ module Clients
 
     def single(url, _, options)
       curl = Curl::Easy.new(url)
+      curl.set(:HTTP_VERSION, Curl::HTTP_1_1)
       curl.ssl_verify_peer = false
       curl.ssl_verify_host = 0
       curl.http_get
 
       curl.status
+    end
+
+    def persistent(url, calls, options)
+      multi = Curl::Multi.new
+      status = []
+      Curl::Multi.get([url] * calls, ssl_verify_peer: false, ssl_verify_host: 0) do |easy|
+        easy.set(:HTTP_VERSION, Curl::HTTP_1_1)
+        status << easy.status
+      end
+      status
     end
 
     def concurrent(url, calls, options)
--- clients/typhoeus.rb
+++ clients/typhoeus.rb
@@ -9,28 +9,21 @@ module Clients
     end
 
     def single(url, _, options)
-      response = Typhoeus::Request.get(url, ssl_verifyhost: 0, ssl_verifypeer: false)
+      response = Typhoeus::Request.get(url, ssl_verifyhost: 0, ssl_verifypeer: false, http_version: :httpv1_1)
 
       response.code
     end
 
     def persistent(url, calls, options)
-      hydra = Typhoeus::Hydra.new(max_concurrency: 3)
-      requests = calls.times.map do
-        request = Typhoeus::Request.new(url, ssl_verifyhost: 0, ssl_verifypeer: false)
-        hydra.queue(request)
-        request
-      end
-      hydra.run
-
-      requests.map(&:response).map(&:code)
+      concurrent(url, calls, options.merge(http_options: {http_version: :httpv1_1}))
     end
 
     def concurrent(url, calls, options)
+      http_options = options.fetch(:http_options, {})
       hydra = Typhoeus::Hydra.new(max_concurrency: 3)
 
       requests = calls.times.map do
-        request = Typhoeus::Request.new(url, ssl_verifyhost: 0, ssl_verifypeer: false)
+        request = Typhoeus::Request.new(url, ssl_verifyhost: 0, ssl_verifypeer: false, **http_options)
         hydra.queue(request)
         request
       end
