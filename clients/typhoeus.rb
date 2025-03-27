8c77e5081cc38700e0aa802c0a2decf53cd0995e
HoneyryderChuck <cardoso_tiago@hotmail.com>
2025-03-27 17:40:41 +0000

    setting pipewait and max_host_connections curl options in typhoeus
    for the same reason as in curb, apples to apples comparison; in the process, managed to make pipelining work, so enabled that as well

--- clients/typhoeus.rb
+++ clients/typhoeus.rb
@@ -23,10 +23,16 @@ module Clients
       concurrent(url, calls, options.merge(hydra_options: {max_concurrency: 1}, http_options: {http_version: :httpv1_1}))
     end
 
+    def pipelined(url, calls, options)
+      concurrent(url, calls, options.merge( http_options: {http_version: :httpv1_1}))
+    end
+
     def concurrent(url, calls, options)
       hydra_options = options.delete(:hydra_options) || {}
       http_options = http_options(options)
       hydra = Typhoeus::Hydra.new(hydra_options)
+      # hack to set this, as there's no way to set this defined option via public API
+      Ethon::Curl.set_option(:max_host_connections, 1, hydra.multi.handle, :multi)
 
       requests = calls.times.map do
         request = Typhoeus::Request.new(url, **http_options)
@@ -42,6 +48,7 @@ module Clients
       http_options = options.fetch(:http_options, {})
       http_options[:ssl_verifyhost] = 0
       http_options[:ssl_verifypeer] = false
+      http_options[:pipewait] = 237 # CURLOPT_PIPEWAIT
       if options[:debug]
         http_options[:verbose] = true
       end
